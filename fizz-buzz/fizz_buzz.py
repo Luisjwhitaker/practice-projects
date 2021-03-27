109847067a600aed747b7fc3fa51d55baaa53860
Luisjwhitaker <luisjwhitaker@gmail.com>
2021-03-27 14:25:43 +0800

    working version with an example print, still needs docstring.

--- fizz-buzz/fizz_buzz.py
+++ fizz-buzz/fizz_buzz.py
@@ -1,15 +1,17 @@
-def fizz_buzz(fizz_number, buzz_number, ran=100):
-    for i in range(1,ran+1):
-        answer= []
+def fizz_buzz(fizz_number, buzz_number, game_range=100):
+    list_return = []
+    for i in range(1,game_range+1):
+        answer= ''
         value=0
         if i % fizz_number == 0:
-            answer += ['fizz ']
+            answer += 'fizz '
             value=1
         if i % buzz_number == 0:
-            answer += ['buzz']
+            answer += 'buzz'
             value=1
         if value == 0:
-            answer += [i]
-        print(answer)
+            answer += str(i)
+        list_return+=[answer]
+    return list_return
 
-fizz_buzz(3,5,50)
+print(fizz_buzz(3,5,50))
