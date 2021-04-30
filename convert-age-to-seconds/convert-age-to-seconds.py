40af6ce56fc0323c100a870ed70de6daba87eeb5
luisjwhitaker <luisjwhitaker@gmail.com>
2021-05-01 03:53:40 +0800

    added more comments

--- convert-age-to-seconds/convert-age-to-seconds.py
+++ convert-age-to-seconds/convert-age-to-seconds.py
@@ -1,4 +1,4 @@
-user_input = 21 #input("Input age")
+user_input = 21 #input("Input age: ")
 age = int(user_input)
 print(31536000*age)
 # in the future i would like to add a calendar day and count the seconds from then.
--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,7 +1,7 @@
 def twoSum(nums,target):
     answer=[]
     for i,num in enumerate(nums):
-        if target-num in nums and target-num != num:
+        if target-num in nums and target-num != num: # temporary fix
             answer+=[i]
     return answer
 print(twoSum([3,2,4],6))
